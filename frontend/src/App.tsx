f9cdf82345b1dc05079937dafda630e6f2c6dda2
Mortez-usa <hatam.info@gmail.com>
2023-10-24 09:26:20 -0700

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+	return (
+		<>
+			<div>
+				<header>Gravi</header>
+				<main></main>
+				<footer>All rights reserved.</footer>
+			</div>
+		</>
+	);
+}
+
+export default App;
