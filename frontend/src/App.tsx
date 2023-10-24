9261c9f87ca2d3d107bb69551f44acaf61a676d2
Mortez-usa <hatam.info@gmail.com>
2023-10-24 11:57:08 -0700

    bootstrap added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,45 @@
-import './App.css';
+import { Container, Navbar } from 'react-bootstrap';
 import { sampleProducts } from './data.tsx';
 
 function App() {
-
 	return (
 		<>
-			<div>
-				<header>Gravi</header>
+			<div className='d-flex flex-column vh-100'>
+				<header>
+					<Navbar bg='dark' variant='dark' expand='lg'>
+						<Container>
+							<Navbar.Brand>GRAVI</Navbar.Brand>
+						</Container>
+						<nav className='nav'>
+							<a href='/cart' className='nav-link'>
+								Cart
+							</a>
+							<a href='/Sign-in' className='nav-link'>
+								Sign in
+							</a>
+						</nav>
+					</Navbar>
+				</header>
 				<main>
 					<ul>
 						{sampleProducts.map((product) => (
 							<li key={product.slug}>
-								<img src={product.image} alt={product.name} className='product-image'/>
+								<img
+									src={product.image}
+									alt={product.name}
+									className='product-image'
+								/>
 								<h2>{product.name}</h2>
 								<p>${product.price}</p>
 							</li>
 						))}
 					</ul>
 				</main>
-				<footer>All rights reserved.</footer>
+				<footer>
+					<div className='text-center'>
+						All rights reserved.
+					</div>
+					</footer>
 			</div>
 		</>
 	);
